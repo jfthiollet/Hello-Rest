81e4e816d42b4a44b9ae1cc54d0ec4cf2eec1777
jfthiollet <jeanfrancois.thiollet@gmail.com>
2017-08-26 23:15:51 +0200

    Correction for war packaging

--- src/main/java/hello/HelloRest.java
+++ src/main/java/hello/HelloRest.java
@@ -2,10 +2,17 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class HelloRest {
+public class HelloRest extends SpringBootServletInitializer {
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(HelloRest.class);
+	    }
 
     public static void main(String[] args) {
     	SpringApplication.run(HelloRest.class, args);

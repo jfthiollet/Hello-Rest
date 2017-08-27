4c5ba69c95f3d65f48ea1ebd2488797cdcb95e7d
jfthiollet <jeanfrancois.thiollet@gmail.com>
2017-08-27 17:48:27 +0200

    Rollback to fat jar

--- src/main/java/hello/HelloRest.java
+++ src/main/java/hello/HelloRest.java
@@ -2,20 +2,27 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-
 
 @SpringBootApplication
-public class HelloRest extends SpringBootServletInitializer {
+public class HelloRest {
 	
-	 @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        return application.sources(HelloRest.class);
-	    }
-
     public static void main(String[] args) {
     	SpringApplication.run(HelloRest.class, args);
          
     }
 }
+/* For war packaging
+public class HelloRest extends SpringBootServletInitializer
+ @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	 	return application.sources(HelloRest.class);
+    }
+    
+
+
+public static void main(String[] args) {
+	SpringApplication.run(HelloRest.class, args);
+     
+	}
+}
+*/

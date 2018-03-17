c63646610cc10a10d5f8bab7d71d23ccfaec9607
jfthiollet <jeanfrancois.thiollet@gmail.com>
2018-03-17 21:55:32 +0100

    Add application.properties

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+	@Value("${greeting.msg1}")
+	private String hello;
+//    private String template = hello + ", %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(method = RequestMethod.GET, value = "/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	String template = hello + ", %s!";
+    	
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
